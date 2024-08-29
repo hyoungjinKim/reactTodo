b7500b31a6079574ba3401fbd151ed0b490f68d6
HYJIN\isacc <isacc7224@gmail.com>
2024-08-29 22:39:53 +0900

    no message

--- client/src/component/Form.js
+++ client/src/component/Form.js
@@ -5,20 +5,19 @@ const Form = ({ handleSubmit, setValue, value }) => {
 
   return (
     <>
-      <form style={{ display: "flex" }} onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="flex pt-2">
         <input
           type="text"
           name="value"
-          style={{ flex: "10", padding: "5px" }}
           placeholder="해야 할 일을 입력하세요"
           value={value}
           onChange={handleChange}
+          className="w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow"
         />
         <input
           type="submit"
           vlaue="입력"
-          className="btn"
-          style={{ flex: "1" }}
+          className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-200"
         />
       </form>
     </>
